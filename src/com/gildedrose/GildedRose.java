0b145365c8c65780d2dad2b2f6266ecee35730a4
Jon Wettersten <jwettersten@ideo.com>
2014-09-23 10:33:50 -0500

    added comments that explain each if/else conditional

--- src/com/gildedrose/GildedRose.java
+++ src/com/gildedrose/GildedRose.java
@@ -1,7 +1,7 @@
 package com.gildedrose;
 
 class GildedRose {
-    Item[] items;
+    Item[] items; // change this type - to objects that inmplement my new interface 
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -9,38 +9,41 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
+        	
+        	// Goal: items[0].updateSellinAndQuality();
+
+            if (!items[i].name.equals("Aged Brie") // filter out brie
+                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // filter out Sulfuras
+                if (items[i].quality > 0) { // Quality can never be negative
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // filter out Sulfuras
+                        items[i].quality = items[i].quality - 1; // decrement quality
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+                if (items[i].quality < 50) { // quality is less than 50 (can never be more than 50)
+                    items[i].quality = items[i].quality + 1; // increment quality
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                               items[i].quality = items[i].quality + 1;
+                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // Backstage passes
+                        if (items[i].sellIn < 11) { // Quality increases by 2 when there are 10 days or less
+                            if (items[i].quality < 50) { // quality is less than 50 (can never be more than 50)
+                               items[i].quality = items[i].quality + 1; // increment quality
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
+                        if (items[i].sellIn < 6) { // Quality increases by 3 when there are 5 days or less
+                            if (items[i].quality < 50) { // quality is less than 50 (can never be more than 50)
+                                items[i].quality = items[i].quality + 1; // increment quality
                             }
                         }
                     }
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) { // filter out Sulfuras
+                items[i].sellIn = items[i].sellIn - 1; // decrement sellIn
             }
 
-            if (items[i].sellIn < 0) { // negative sellin items
+            if (items[i].sellIn < 0) { // negative sellin items (quality should degrade twice as fast
                 if (!items[i].name.equals("Aged Brie")) { // filter out brie
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) { // filter out passes
                         if (items[i].quality > 0) { // quality is still non-negative
@@ -48,12 +51,12 @@ class GildedRose {
                                 items[i].quality = items[i].quality - 1; // decrement this item's quality
                             }
                         }
-                    } else {
+                    } else { // quality degrades twice as fast at this point
                         items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else { // brie
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
+                    if (items[i].quality < 50) { // quality is never more than 50
+                        items[i].quality = items[i].quality + 1; // increment quality
                     }
                 }
             }
